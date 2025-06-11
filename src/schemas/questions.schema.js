f8b05bb338b735a2fd101e1fb47e50696a3b9339
M-HASNAIN-ALI-SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-06-11 23:06:48 +0500

    added questions schema

---
+++ src/schemas/questions.schema.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const questionsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    enum: [
+      "general",
+      "technical",
+      "behavioral",
+      "short",
+      "paragraph",
+      "multiple-choice",
+      "dropdown",
+      "check-box",
+      "linear-scale",
+      "date",
+      "time",
+    ],
+  },
+  options: [
+    {
+      type: String,
+    },
+  ],
+  formId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Form",
+    required: true,
+  },
+});
+
+const Questions = mongoose.model("Question", questionsSchema);
+
+export default Questions;
