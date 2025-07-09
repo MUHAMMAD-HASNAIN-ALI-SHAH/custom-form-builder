c62ed081ff3857ff5eed87f70f4dbbac8ad02591
M-HASNAIN-ALI-SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-07-09 16:44:15 +0500

    Form link added

--- src/components/Dashboard/MyForms/Forms.tsx
+++ src/components/Dashboard/MyForms/Forms.tsx
@@ -1,34 +1,50 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import useFormStore from "@/store/useFormStore";
-import React, { useEffect } from "react";
+import FormLink from "./FormLink";
 
 const Forms = () => {
   const { forms, getForms } = useFormStore();
+  const [openLink, setOpenLink] = useState<string | null>(null);
+
   useEffect(() => {
     getForms();
   }, [getForms]);
+
   return (
     <>
+      {openLink && (
+        <FormLink
+          link={`http://localhost:3000/form/${openLink}`}
+          onClose={() => setOpenLink(null)}
+        />
+      )}
+
       {forms && forms.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6">
           {forms.map((form, index) => (
             <div
               key={index}
-              className="flex flex-col bg-white shadow-md rounded-lg h-60"
+              className="flex flex-col bg-white shadow-md rounded-lg h-72"
             >
               {/* Top 60%: Icon */}
-              <div className="h-[60%] flex items-center justify-center text-9xl">
+              <div className="h-[50%] flex items-center justify-center text-9xl">
                 📝
               </div>
 
               {/* Bottom 40%: Name, Responses, and Menu */}
-              <div className="h-[40%] flex flex-col justify-between p-4">
+              <div className="h-[50%] flex flex-col justify-between p-4">
                 <div>
                   <div className="text-lg font-semibold">{form.title}</div>
                   <div className="text-gray-500 text-sm">0 responses</div>
                 </div>
-                <div className="flex justify-end text-2xl text-gray-900 cursor-pointer">
-                  ⋯
+                <div className="flex justify-end py-3 mt-4">
+                  <button
+                    onClick={() => setOpenLink(form._id)}
+                    className="text-sm text-blue-600 hover:underline"
+                  >
+                    Share Link
+                  </button>
                 </div>
               </div>
             </div>
